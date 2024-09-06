82d624de2c917c2047b81da82418da86cb611d39
Lord Lendalot <lord_lendalot@lendasat.com>
2024-09-06 16:38:05 +1000

    chore: minor changes to the hero section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,14 @@
-import { GoArrowUpRight } from 'react-icons/go';
-import Bitcoin from './../assets/icons/bitcoin.png';
-import Icon from './../assets/icons/icon.png';
-import { Link } from 'react-router-dom';
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
+import {GoArrowUpRight} from 'react-icons/go';
+import {Link} from 'react-router-dom';
+import {gsap} from "gsap";
+import {useGSAP} from "@gsap/react";
 
 
 gsap.registerPlugin(useGSAP);
 export const Banner = () => {
 
     useGSAP(() => {
-        const handleAfterLoading = () => {
+        window.onload = () => {
             gsap.from('.icon', {
                 translateY: 50,
                 opacity: 0,
@@ -28,28 +26,31 @@ export const Banner = () => {
             })
 
         }
-
-        window.onload = handleAfterLoading
     }, [])
     return (
-        <section className='pt-48'>
-            <div className="text-center">
-                <div className=" flex items-center justify-center gap-3">
-                    <img className='h-10 w-auto icon' src={Bitcoin} alt="Icon" />
-                    <img className='h-10 w-auto icon' src={Icon} alt="Icon" />
-                </div>
-                <h1 className='mb-14 heading'>
-                    Generate interest <br />
-                    through bitcoin-backed loans
-                </h1>
-                <p className='text-base md:text-xl lg:text-2xl text-[#595959] heading'>Lendasat lets you take instant loans to pay Lightning invoices. No need to sell your Bitcoin.</p>
-                <div className="flex items-center justify-center gap-4 mt-8">
-                    <Link to="#subscribe" className='text-white flex items-center gap-2 px-8 py-4 rounded-full bg-primary hover:bg-black transition-colors ease-linear duration-300 heading'>
-                        <span>Try It</span> <GoArrowUpRight className='arrow_movement' />
-                    </Link>
-                    <Link to="#how_it_works" className='text-[#2E1B52] flex items-center gap-2 px-8 py-4 rounded-full bg-[#F3F3F3] hover:bg-[#4E2E8D] hover:text-white transition-colors ease-linear duration-300 heading'>
-                        <span>Learn More</span> <GoArrowUpRight className='arrow_movement' />
-                    </Link>
+        <section className='min-h-screen flex items-center justify-center bg-blue-100 pt-30 px-4 pb-4'>
+            <div
+                className="w-[95vw] h-[95vh] border-purple-200 rounded-3xl flex items-center justify-center bg-opacity-95 shadow-lg">
+                <div className="text-center max-w-4xl w-full mx-auto p-4">
+                    <h1 className='mb-14 heading text-4xl md:text-5xl lg:text-6xl font-bold'>
+                        You've hodled enough.<br/>
+                        <span className="text-primary">Start living, pay later.</span>
+                    </h1>
+                    <p className='text-base md:text-xl lg:text-2xl text-[#595959] heading mb-8'>
+                        Borrow against your Bitcoin in a secure and non-custodial way. <br/>
+                        Never. Sell. Your. Bitcoin.
+                    </p>
+                    <div className="flex flex-col sm:flex-row items-center justify-center gap-4 mt-8">
+                        <Link to="#subscribe"
+                              className='w-full sm:w-auto text-white flex items-center justify-center gap-2 px-8 py-4 rounded-full bg-primary hover:bg-black transition-colors ease-linear duration-300 heading'>
+                            <span>Try It</span> <GoArrowUpRight className='arrow_movement'/>
+                        </Link>
+                        <Link to="https://whitepaper.lendasat.com/lendasat-whitepaper.pdf" target="_blank"
+                              rel="noopener noreferrer"
+                              className='w-full sm:w-auto text-[#2E1B52] flex items-center justify-center gap-2 px-8 py-4 rounded-full bg-[#F3F3F3] hover:bg-[#4E2E8D] hover:text-white transition-colors ease-linear duration-300 heading'>
+                            <span>Learn More</span> <GoArrowUpRight className='arrow_movement'/>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </section>
